db487c5590c34d3b61bc08a9b44bdbedad677623
gizemilhn <ilhangiz3m@gmail.com>
2023-03-04 14:53:11 +0300

    Basit Not Tutma Uygulaması
    Mobil Uygulama dersinde Kotlin üzerinden proje olarak geliştirdiğim not tutma uygulamasına ait kodlar.

---
+++ NotApp/app/src/main/java/com/example/notapp/Database/NoteDatabase.kt
@@ -0,0 +1,40 @@
+package com.example.notapp.Database
+
+import android.content.Context
+import androidx.room.Database
+import androidx.room.Room
+import androidx.room.RoomDatabase
+import com.example.notapp.Models.Note
+import com.example.notapp.utilities.DATABASE_NAME
+
+
+@Database(entities = arrayOf(Note::class), version = 1, exportSchema = false)
+abstract class NoteDatabase:RoomDatabase()  {
+
+    abstract fun  getNoteDao() : NoteDao
+
+    companion object{
+
+        @Volatile
+        private var INSTANCE :NoteDatabase? = null
+
+        fun getDatabase(context : Context) : NoteDatabase{
+            return INSTANCE?: synchronized(this){
+                val instance = Room.databaseBuilder(
+                    context.applicationContext,
+                    NoteDatabase::class.java,
+                    DATABASE_NAME
+                ).build()
+
+                INSTANCE = instance
+                instance
+
+
+
+
+            }
+        }
+    }
+
+
+}
---
+++ NotApp/app/src/main/java/com/example/notapp/utilities/constants.kt
@@ -0,0 +1,3 @@
+package com.example.notapp.utilities
+
+const val DATABASE_NAME = "note_database"
